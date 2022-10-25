329cccb7d247861b2adb2e6411d9e2512efef3a6
Hassen Ahmed <osahfaxo8@gmail.com>
2022-10-25 16:44:46 +0300

    Added sortColors.js

---
+++ sortcolors.js
@@ -0,0 +1,16 @@
+const sortColors = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let min = arr[i];
+    let pos = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        min = arr[j];
+        pos = j;
+        let temp = arr[i];
+        arr[i] = min;
+        arr[pos] = temp;
+      }
+    }
+  }
+  return arr;
+};
