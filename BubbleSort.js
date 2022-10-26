4de3b8e514461194ca612acc9bec6c99b9293458
Hassen Ahmed <osahfaxo8@gmail.com>
2022-10-26 20:36:54 +0300

    Updated BubbleSort.js

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,16 +1,19 @@
 const countSwaps = (a) => {
-  let count = 0;
+  let numSwaps = 0;
   for (let i = 0; i < a.length; i++) {
     for (let j = 0; j < a.length - 1; j++) {
       if (a[j] > a[j + 1]) {
-        let temp = a[j];
-        a[j] = a[j + 1];
-        a[j + 1] = temp;
-        count++;
+        [a[j], a[j + 1]] = [a[j + 1], a[j]];
+        numSwaps++;
       }
     }
   }
-  console.log("Array is sorted in " + count + " swaps.");
-  console.log("First Element: " + a[0]);
-  console.log("Last Element: " + a[a.length - 1]);
+  console.log(
+    "Array is sorted in " +
+      numSwaps +
+      " swaps.\nFirst Element: " +
+      a[0] +
+      "\nLast Element: " +
+      a[a.length - 1]
+  );
 };
