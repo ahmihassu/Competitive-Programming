bf3bc3d8e359e17ae3e1a949336f3f180829a434
Hassen Ahmed <osahfaxo8@gmail.com>
2022-10-27 17:42:47 +0300

    Updated InsertonSort.js

--- InsertionSort.js
+++ InsertionSort.js
@@ -1,17 +1,17 @@
 const insertionSort1 = (n, arr) => {
-  let rightmost = arr[n - 1];
-  for (let i = n - 1; i >= 0; i--) {
-    if (rightmost < arr[i - 1]) {
-      arr[i] = arr[i - 1];
-      arr.forEach((element) => {
-        process.stdout.write(element.toString() + " ");
-      });
-      console.log();
+  const last = arr[n - 1];
+  for (let i = n - 2; i >= 0; i--) {
+    if (last <= arr[i]) {
+      [arr[i + 1]] = [arr[i]]; //copy using destruct
+      console.log(arr.join(" "));
+      if (i == 0) {
+        arr[i] = last;
+        console.log(arr.join(" "));
+        break;
+      }
     } else {
-      arr[i] = rightmost;
-      arr.forEach((element) => {
-        process.stdout.write(element.toString() + " ");
-      });
+      arr[i + 1] = last;
+      console.log(arr.join(" "));
       break;
     }
   }
