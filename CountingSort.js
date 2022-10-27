e0955d20c37bda8d0ecd68e7b70083690c13d221
Hassen Ahmed <osahfaxo8@gmail.com>
2022-10-27 18:05:53 +0300

    Updated CountingSort.js

--- CountingSort.js
+++ CountingSort.js
@@ -1,19 +1,7 @@
 const countingSort = (arr) => {
-  let freq = [];
-  let counted = [];
-  for (let i = 0; i < arr.length; i++) {
-    freq[i] = 0;
-    counted[i] = false;
+  let freq = Array(100).fill(0, 0);
+  for (let el of arr) {
+    freq[el]++;
   }
-  for (let i = 0; i < arr.length; i++) {
-    if (counted[i] == true) continue;
-    for (let j = 0; j < arr.length; j++) {
-      let pos = arr[i];
-      if (arr[i] === arr[j]) {
-        freq[pos]++;
-        counted[j] = true;
-      }
-    }
-  }
-  return freq.slice(0, 100);
+  return freq;
 };
