b0ad63048e76c27178d0d7adaccf0678fb0ae966
Hassen Ahmed <osahfaxo8@gmail.com>
2022-11-05 20:58:15 +0300

    Added pivotIndex.js

---
+++ pivotIndex.js
@@ -0,0 +1,18 @@
+const pivotIndex = (nums) => {
+  let rightSum = 0;
+  let leftSum = 0;
+
+  nums.forEach((v) => (rightSum += v));
+
+  for (var i = 0; i <= nums.length - 1; i++) {
+    let curr = nums[i];
+
+    rightSum -= curr;
+
+    if (leftSum === rightSum) return i;
+
+    leftSum += curr;
+  }
+
+  return -1;
+};
