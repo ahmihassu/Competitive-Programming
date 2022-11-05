e558ad9d4a3a62cc20767dc1a9991337c888740d
Hassen Ahmed <osahfaxo8@gmail.com>
2022-11-05 20:41:16 +0300

    Added Maximum sum of two non overlapping subarrays

---
+++ maxSumTwoNoOverlap.js
@@ -0,0 +1,32 @@
+const maxSumTwoNoOverlap = (nums, firstLen, secondLen) => {
+  let firstSum = [];
+  let secondSum = [];
+  let currSumF = 0;
+  let currSumS = 0;
+  let ans = 0;
+  for (var i = 0; i < nums.length; i++) {
+    currSumF += nums[i];
+    currSumS += nums[i];
+    if (i - firstLen + 1 >= 0) {
+      firstSum.push(currSumF);
+      currSumF -= nums[i - firstLen + 1];
+    }
+    if (i - secondLen + 1 >= 0) {
+      secondSum.push(currSumS);
+      currSumS -= nums[i - secondLen + 1];
+    }
+  }
+  for (var i = 0; i < firstSum.length; i++) {
+    for (var j = i + firstLen; j < secondSum.length; j++) {
+      currSumF = firstSum[i] + secondSum[j];
+      ans = Math.max(ans, currSumF);
+    }
+  }
+  for (var i = 0; i < secondSum.length; i++) {
+    for (var j = i + secondLen; j < firstSum.length; j++) {
+      currSumS = secondSum[i] + firstSum[j];
+      ans = Math.max(ans, currSumS);
+    }
+  }
+  return ans;
+};
